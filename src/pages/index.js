03a91e16b87eb766c32970cde612265542ec8cab
M1HUEL <imiguelx008@gmail.com>
2022-11-14 21:24:31 -0700

    #

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import styles from "../styles/modules/Home.module.scss";
+
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Cyom - Website</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div></div>
+    </>
+  );
+}
